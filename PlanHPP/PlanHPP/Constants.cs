fd314716b0eba07c31d7b649adc504ed0115c0d0
Андрей Шкаликов <kasrkin212@gmail.com>
2022-03-26 22:55:02 +0300

    Change DNS

--- PlanHPP/PlanHPP/Constants.cs
+++ PlanHPP/PlanHPP/Constants.cs
@@ -8,9 +8,9 @@ namespace PlanHPP
     public static class Constants
     {
         //10.0.2.2:5000
-        //a5464-b309.a.d-f.pw
-        public static string DataRestUrl = DeviceInfo.Platform == DevicePlatform.Android ? "https://a5464-b309.a.d-f.pw/api/Motor/" : "https://a5464-b309.a.d-f.pw/api/Motor/";
-        public static string UserRegistrationRestUrl = DeviceInfo.Platform == DevicePlatform.Android ? "https://a5464-b309.a.d-f.pw/api/User/register/" : "https://a5464-b309.a.d-f.pw/api/User/register/";
-        public static string UserAuthicationRestUrl = DeviceInfo.Platform == DevicePlatform.Android ? "https://a5464-b309.a.d-f.pw/api/User/login/" : "https://a5464-b309.a.d-f.pw/api/User/login/";
+        //a5540-1dc5.b.d-f.pw
+        public static string DataRestUrl = DeviceInfo.Platform == DevicePlatform.Android ? "https://a5540-1dc5.b.d-f.pw/api/Motor/" : "https://a5540-1dc5.b.d-f.pw/api/Motor/";
+        public static string UserRegistrationRestUrl = DeviceInfo.Platform == DevicePlatform.Android ? "https://a5540-1dc5.b.d-f.pw/api/User/register/" : "https://a5540-1dc5.b.d-f.pw/api/User/register/";
+        public static string UserAuthicationRestUrl = DeviceInfo.Platform == DevicePlatform.Android ? "https://a5540-1dc5.b.d-f.pw/api/User/login/" : "https://a5540-1dc5.b.d-f.pw/api/User/login/";
     }
 }
